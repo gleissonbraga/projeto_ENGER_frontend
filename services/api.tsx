10a8bfcda4ae933962156ba8149866a7ef980942
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2026-06-19 20:09:34 -0300

    fix: corrigindo case do arquivo de relatorios

--- services/api.tsx
+++ services/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrll = process.env.NEXT_PUBLIC_API_URL;
+const apiUrll = "https://enger-api.onrender.com/api";
 
 const api = axios.create({
   baseURL: apiUrll, // Substitua pela porta do seu backend ENGER
